f60f648a4dcecad4ddf8a3586befb1a100a78d23
Hydrostatik <40757442+Hydrostatik@users.noreply.github.com>
2024-11-13 23:56:40 -0500

    Integrate http4s and implement `/fruits` endpoint

--- build.sbt
+++ build.sbt
@@ -7,22 +7,33 @@ ThisBuild / scalaVersion := "3.5.2"
 // See https://github.com/typelevel/sbt-tpolecat/?tab=readme-ov-file#modes
 ThisBuild / tpolecatDefaultOptionsMode := VerboseMode
 
+val catsVersion = "3.5.3"
+val doobieVersion = "1.0.0-RC5"
+val http4sVersion = "0.23.29"
+
 lazy val root = (project in file(".")).settings(
   name := "scala_htmx_demo",
   libraryDependencies ++= Seq(
     "org.typelevel" %% "cats-core" % "2.12.0",
     // "core" module - IO, IOApp, schedulers
     // This pulls in the kernel and std modules automatically.
-    "org.typelevel" %% "cats-effect" % "3.5.3",
+    "org.typelevel" %% "cats-effect" % catsVersion,
     // concurrency abstractions and primitives (Concurrent, Sync, Async etc.)
-    "org.typelevel" %% "cats-effect-kernel" % "3.5.3",
+    "org.typelevel" %% "cats-effect-kernel" % catsVersion,
     // standard "effect" library (Queues, Console, Random etc.)
-    "org.typelevel" %% "cats-effect-std" % "3.5.3",
+    "org.typelevel" %% "cats-effect-std" % catsVersion,
     "org.typelevel" %% "munit-cats-effect" % "2.0.0" % Test,
     // Functional JDBC library for Scala
-    "org.tpolecat" %% "doobie-core" % "1.0.0-RC5",
-    "org.tpolecat" %% "doobie-hikari" % "1.0.0-RC5",
-    "org.tpolecat" %% "doobie-postgres" % "1.0.0-RC5",
-    "org.postgresql" % "postgresql" % "42.5.3"
+    "org.tpolecat" %% "doobie-core" % doobieVersion,
+    "org.tpolecat" %% "doobie-hikari" % doobieVersion,
+    "org.tpolecat" %% "doobie-postgres" % doobieVersion,
+    "org.postgresql" % "postgresql" % "42.5.3",
+    // Functional Http server and client library
+    "org.http4s" %% "http4s-dsl" % http4sVersion,
+    "org.http4s" %% "http4s-ember-server" % http4sVersion,
+    "org.http4s" %% "http4s-ember-client" % http4sVersion,
+    // JSON encoding/decoding
+    "org.http4s" %% "http4s-circe" % http4sVersion,
+    "io.circe" %% "circe-generic" % "0.14.8"
   )
 )
--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,10 +1,18 @@
 package com.example
 
-import cats.effect.{IO, Resource}
+import cats.effect._
 import doobie._
 import doobie.hikari._
 import doobie.implicits._
-import cats.effect.unsafe.implicits.global
+import com.comcast.ip4s._
+import org.http4s.ember.server._
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server.Router
+import io.circe.generic.auto._
+import org.http4s.circe._
+import org.http4s.circe.CirceEntityEncoder.circeEntityEncoder
 
 object Database {
   val host = "localhost"
@@ -17,35 +25,42 @@ object Database {
 
   def transactor: Resource[IO, HikariTransactor[IO]] = {
     HikariTransactor.newHikariTransactor[IO](
-      driverClassName = "org.postgresql.Driver",
       url = dbUrl,
       user = dbUser,
       pass = dbPassword,
-      connectEC = ExecutionContexts.synchronous
+      connectEC = ExecutionContexts.synchronous,
+      driverClassName = "org.postgresql.Driver"
     )
   }
 }
 
-case class Fruit(id: Int, name: String, quantity: Int, pricePerPound: Int)
-
-@main
-def run() =
-  // Print the database URL to verify it
-  println(s"Database URL: ${Database.dbUrl}")
-
-  // Example query to check the connection
-  val query = sql"SELECT id, name, quantity, pricePerPound FROM inventory"
-    .query[Fruit]
-
-  val program = Database.transactor.use { xa =>
-    query.to[List].transact(xa).flatMap { inventoryList =>
-      IO {
-        inventoryList.foreach { inventory =>
-          println(
-            s"ID: ${inventory.id}, Name: ${inventory.name}, Quantity: ${inventory.quantity}, Price per Pound: ${inventory.pricePerPound}"
-          )
-        }
-      }
-    }
+def getFruits: IO[List[Fruit]] =
+  val query =
+    sql"SELECT id, name, quantity, pricePerPound FROM inventory".query[Fruit]
+  Database.transactor.use { xa =>
+    query.to[List].transact(xa)
   }
-  program.unsafeRunSync()
+
+case class Fruit(id: Int, name: String, quantity: Int, pricePerPound: Int)
+implicit val fruitEncoder: EntityEncoder[IO, Fruit] = jsonEncoderOf[IO, Fruit]
+
+val fruitService = HttpRoutes.of[IO] { case GET -> Root / "fruits" =>
+  for {
+    fruits <- getFruits
+    response <- Ok(fruits)
+  } yield response
+}
+
+val httpApp = Router("/api" -> fruitService).orNotFound
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(httpApp)
+      .build
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
+}
